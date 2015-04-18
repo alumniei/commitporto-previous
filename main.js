3db5771b5afa52945ab7d5c353e4dba9a695185b
Ana Carraca <ana.carraca.72@gmail.com>
2015-04-18 03:49:01 +0100

    Venue section added

--- main.js
+++ main.js
@@ -11,3 +11,21 @@ $( document ).ready(function() {
         $("#mc-embedded-subscribe").removeClass("subscribe");
     });
 });
+
+function initializeLocationMap() {
+  var Latlng = new google.maps.LatLng(41.177875,-8.597916);
+  var markerImg = "images/map-marker.png";
+  var mapOptions = {
+    zoom: 16,
+    center: Latlng
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: Latlng,
+      map: map,
+      icon: markerImg
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initializeLocationMap);
