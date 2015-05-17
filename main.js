68118797be6ffeeec71498447569e3f0c2a9b4b2
maria miranda <mariajplmiranda@gmail.com>
2015-05-17 23:24:59 +0100

    menu scroll

--- main.js
+++ main.js
@@ -31,3 +31,43 @@ function initializeLocationMap() {
 }
 
 google.maps.event.addDomListener(window, 'load', initializeLocationMap);
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('.top-bar a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.top-bar ul li a').removeClass("active");
+            currLink.parent().addClass("active");
+        }
+        else{
+            currLink.parent().removeClass("active");
+        }
+    });
+}
