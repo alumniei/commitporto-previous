d691778d63c3bd4b05fa4b3793a7431a42e653d1
maria miranda <mariajplmiranda@gmail.com>
2015-05-17 23:34:45 +0100

    hide header first page

--- main.js
+++ main.js
@@ -1,38 +1,25 @@
-$( document ).ready(function() {
-    setTimeout(function(){$("#btn-subscribe").removeClass("noshow")}, 1000)
-    $("#btn-subscribe").click(function(){
-        setTimeout(function(){$("#mce-EMAIL").addClass("subscribe")}, 400);
-        setTimeout(function(){$("#mc-embedded-subscribe").addClass("subscribe")}, 400);
-        $("#btn-subscribe").addClass("noshow");
-    });
-    
-    $("#mc-embedded-subscribe").click(function(){
-        $("#mce-EMAIL").removeClass("subscribe");
-        $("#mc-embedded-subscribe").removeClass("subscribe");
-    });
-});
-
 function initializeLocationMap() {
-  var Latlng = new google.maps.LatLng(41.177875,-8.597916);
-  var markerImg = "images/map.svg";
-    
-  var mapOptions = {
-    zoom: 16,
-    center: Latlng,
-    scrollwheel: false
-  }
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var Latlng = new google.maps.LatLng(41.177875,-8.597916);
+    var markerImg = "images/map.svg";
+      
+    var mapOptions = {
+      zoom: 16,
+      center: Latlng,
+      scrollwheel: false
+    }
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-  var marker = new google.maps.Marker({
-      position: Latlng,
-      map: map,
-      icon: markerImg
-  });
+    var marker = new google.maps.Marker({
+        position: Latlng,
+        map: map,
+        icon: markerImg
+    });
 }
 
 google.maps.event.addDomListener(window, 'load', initializeLocationMap);
 
 $(document).ready(function () {
+    var height =
     $(document).on("scroll", onScroll);
     
     //smoothscroll
@@ -59,6 +46,13 @@ $(document).ready(function () {
 
 function onScroll(event){
     var scrollPos = $(document).scrollTop();
+
+    if (scrollPos > $('.header .date').offset().top) {
+        $('.top-bar').show();
+    } else {
+        $('.top-bar').hide();
+    }
+
     $('.top-bar a').each(function () {
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
