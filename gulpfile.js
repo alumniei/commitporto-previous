a757523c337a3d32cdb3e6b023c11f4c66d7bd1d
maria miranda <mariajplmiranda@gmail.com>
2016-02-01 22:01:19 +0000

    node modules

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync'),
+	useref = require('gulp-useref'),
+	uglify = require('gulp-uglify'),
+	gulpIf = require('gulp-if'),
+	cssnano = require('gulp-cssnano');
+
+gulp.task('run', ['browser-sync', 'compile-sass', 'concat-minify'], function() {
+	gulp.watch('app/scss/*.scss', ['compile-sass']);
+	gulp.watch('app/*.html', browserSync.reload); 
+  	gulp.watch('app/js/**/*.js', browserSync.reload); 
+})
+
+/** CSS tasks **/
+gulp.task('compile-sass', function(){
+	return gulp.src('app/scss/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({
+	     	stream: true
+	    }))
+});
+
+/** SYNC tasks **/
+gulp.task('browser-sync', function() {
+ 	browserSync({
+	    server: {
+	      baseDir: 'app'
+	    },
+  })
+})
+
+/** CONCAT/MINIFY tasks **/
+gulp.task('concat-minify', function(){
+  	return gulp.src('app/*.html')
+	    .pipe(useref())
+	    .pipe(gulpIf('*.js', uglify()))
+    	.pipe(gulpIf('*.css', cssnano()))
+	    .pipe(gulp.dest('dist'))
+});
