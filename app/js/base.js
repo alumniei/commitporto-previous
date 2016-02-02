b6de7b74258322e63933b6f268de6dbe54fe7bba
maria miranda <mariajplmiranda@gmail.com>
2016-02-02 00:35:54 +0000

    nav bar top

--- app/js/base.js
+++ app/js/base.js
@@ -1,3 +1,40 @@
-if(console.log('ola')) {
-	var a = 'ads';
+(function() {
+	var hash = window.location.hash,
+		nav = document.getElementsByTagName('nav')[0],
+		navHeight  = nav.offsetHeight;
+
+	window.onscroll = function() {
+		if(document.body.scrollTop >=  window.innerHeight - navHeight) {
+			Helpers.removeClass(nav, 'bottom');
+		} else {
+			Helpers.addClass(nav, 'bottom');
+		} 
+	};
+
+
+})();
+
+var Helpers = (function () {
+	'use strict';
+
+	var addClass = function (element, newClass) {
+		if(element.classList && !element.classList.contains(newClass)) {
+			element.classList.add(newClass);
+		}
+		return;
+		},
+		removeClass = function (element, removeClass) {
+			if (element.classList) {
+		        element.classList.remove(removeClass);
+		    }
+		};
+
+	return {
+		addClass: addClass,
+		removeClass: removeClass
+	}
+}());
+
+var Binds = new function () {
+
 }
