3cb08d1352d3d7a933988208ef72d7b608014426
maria miranda <mariajplmiranda@gmail.com>
2016-02-20 18:08:26 +0000

    landing_page

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,13 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	browserSync = require('browser-sync'),
-	useref = require('gulp-useref'),
-	uglify = require('gulp-uglify'),
-	gulpIf = require('gulp-if'),
-	cssnano = require('gulp-cssnano');
-
-gulp.task('run', ['browser-sync', 'compile-sass', 'concat-minify'], function() {
-	gulp.watch('app/scss/*.scss', ['compile-sass']);
-	gulp.watch('app/*.html', browserSync.reload);
-  	gulp.watch('app/js/**/*.js', browserSync.reload);
-});
+	sass = require('gulp-sass');
 
 /** CSS tasks **/
 gulp.task('compile-sass', function(){
-	return gulp.src('app/scss/*.scss')
+	return gulp.src('scss/*.scss')
 		.pipe(sass())
-		.pipe(gulp.dest('app/css'))
-		.pipe(browserSync.reload({
-	     	stream: true
-	    }));
+		.pipe(gulp.dest('css'))
 });
 
-/** SYNC tasks **/
-gulp.task('browser-sync', function() {
- 	browserSync({
-	    server: {
-	      baseDir: 'app'
-	    },
-  });
-});
-
-/** CONCAT/MINIFY tasks **/
-gulp.task('concat-minify', function(){
-  	return gulp.src('app/*.html')
-	    .pipe(useref())
-	    .pipe(gulpIf('*.js', uglify()))
-    	.pipe(gulpIf('*.css', cssnano()))
-	    .pipe(gulp.dest('dist'));
+gulp.task('watch', ['compile-sass'], function() {
+	gulp.watch('scss/*.scss', ['compile-sass']);
 });
