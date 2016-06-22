aefcdabfc7ed559b2111fc7035cb49aca0c2cea8
Eduardo Pinto <efgpinto@gmail.com>
2016-06-22 14:58:31 +0100

    clickable agenda opens speaker details

--- js/base.js
+++ js/base.js
@@ -14,6 +14,14 @@ function hideDetails(name) {
     large.style.display = "none";
 }
 
+function toggleDetails(name) {
+    var elem = document.getElementById(name);
+    if (elem.style.display == "")
+      showDetails(name);
+    else
+      hideDetails(name);
+}
+
 function highlightSpeaker(name) {
   var small = document.getElementById(name);
   var details = small.childNodes[3].childNodes[1];
