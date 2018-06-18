a20df619731ae9c859e343615466c6349bb23ad2
Eduardo Pinto <efgpinto@gmail.com>
2018-06-18 21:08:42 +0100

    add dara

--- js/speakers.js
+++ js/speakers.js
@@ -3,19 +3,21 @@ const speakers = document.getElementsByClassName("speaker");
 Array.from(speakers).forEach(function (speaker) {
   speaker.onclick = function(e) {
     const attributes = e.currentTarget.dataset;
-    document.getElementById('talk').classList.add("active");
-    document.getElementById('speaker-photo').src = e.currentTarget.children[0].src;
-    document.getElementById('speaker-name').innerHTML = e.currentTarget.children[1].innerHTML;
-    document.getElementById('talk-name').innerHTML = attributes.name;
-    document.getElementById('talk-description').innerHTML = attributes.talkdesc;
-    if (attributes.talkvideo) {
-      document.getElementById('talk-video').href = attributes.talkvideo;
-    } else {
-      document.getElementById('talk-video').style.display = 'none';
-    }
-    document.getElementById('bio').innerHTML = attributes.bio;
+    if (attributes.name) {
+      document.getElementById('talk').classList.add("active");
+      document.getElementById('speaker-photo').src = e.currentTarget.children[0].src;
+      document.getElementById('speaker-name').innerHTML = e.currentTarget.children[1].innerHTML;
+      document.getElementById('talk-name').innerHTML = attributes.name;
+      document.getElementById('talk-description').innerHTML = attributes.talkdesc;
+      if (attributes.talkvideo) {
+        document.getElementById('talk-video').href = attributes.talkvideo;
+      } else {
+        document.getElementById('talk-video').style.display = 'none';
+      }
+      document.getElementById('bio').innerHTML = attributes.bio;
 
-    document.getElementById('speakers').scrollIntoView({block: "start", behavior: "smooth"});
+      document.getElementById('speakers').scrollIntoView({block: "start", behavior: "smooth"});
+    }
   }
 });
 
