c57fc50851bbb6ea192913f1f9a0f996fbef780f
Eduardo Pinto <efgpinto@gmail.com>
2018-06-20 22:53:54 +0100

    adding social networks to speakers

--- js/speakers.js
+++ js/speakers.js
@@ -22,6 +22,40 @@ function openPopup(elem) {
     document.getElementById('speaker-name').innerHTML = elem.children[1].innerHTML;
     document.getElementById('talk-name').innerHTML = attributes.name;
     document.getElementById('talk-description').innerHTML = attributes.talkdesc;
+
+    if (attributes.linkedin) {
+      document.getElementById('social-linkedin').href = attributes.linkedin;
+      document.getElementById('social-linkedin').children[0].classList.remove('hidden');
+    } else {
+      document.getElementById('social-linkedin').children[0].classList.add('hidden');
+    }
+
+    if (attributes.twitter) {
+      document.getElementById('social-twitter').href = attributes.twitter;
+      document.getElementById('social-twitter').children[0].classList.remove('hidden');
+    } else {
+      document.getElementById('social-twitter').children[0].classList.add('hidden');
+    }
+
+    if (attributes.github) {
+      document.getElementById('social-github').href = attributes.github;
+      document.getElementById('social-github').children[0].classList.remove('hidden');
+    } else {
+      document.getElementById('social-github').children[0].classList.add('hidden');
+    }
+
+    if (attributes.gitlab) {
+      document.getElementById('social-gitlab').href = attributes.gitlab;
+      document.getElementById('social-gitlab').children[0].classList.remove('hidden');
+    } else {
+      document.getElementById('social-gitlab').children[0].classList.add('hidden');
+    }
+
+    if (attributes.link) {
+      document.getElementById('social-link').href = attributes.link;
+      document.getElementById('social-link').children[0].classList.remove('hidden');
+    }
+
     if (attributes.talkvideo) {
       document.getElementById('talk-video').href = attributes.talkvideo;
     } else {
@@ -32,4 +66,3 @@ function openPopup(elem) {
     document.getElementById('speakers').scrollIntoView({block: "start", behavior: "smooth"});
   }
 }
-
