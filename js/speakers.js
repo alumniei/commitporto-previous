cc58d90ba47122d7611339a65fcdfcafc12a583f
Eduardo Pinto <eduardo.pinto@blip.pt>
2018-06-20 09:07:50 +0100

    add js

--- js/speakers.js
+++ js/speakers.js
@@ -2,25 +2,34 @@ const speakers = document.getElementsByClassName("speaker");
 
 Array.from(speakers).forEach(function (speaker) {
   speaker.onclick = function(e) {
-    const attributes = e.currentTarget.dataset;
-    if (attributes.name) {
-      document.getElementById('talk').classList.add("active");
-      document.getElementById('speaker-photo').src = e.currentTarget.children[0].src;
-      document.getElementById('speaker-name').innerHTML = e.currentTarget.children[1].innerHTML;
-      document.getElementById('talk-name').innerHTML = attributes.name;
-      document.getElementById('talk-description').innerHTML = attributes.talkdesc;
-      if (attributes.talkvideo) {
-        document.getElementById('talk-video').href = attributes.talkvideo;
-      } else {
-        document.getElementById('talk-video').style.display = 'none';
-      }
-      document.getElementById('bio').innerHTML = attributes.bio;
-
-      document.getElementById('speakers').scrollIntoView({block: "start", behavior: "smooth"});
-    }
+    openPopup(e.currentTarget);
   }
 });
 
 document.getElementById('close').onclick = function(e) {
   document.getElementById('talk').classList.remove("active");
 }
+
+function openDetail(id) {
+  openPopup(document.getElementById(id))
+}
+
+function openPopup(elem) {
+  const attributes = elem.dataset;
+  if (attributes.name) {
+    document.getElementById('talk').classList.add("active");
+    document.getElementById('speaker-photo').src = elem.children[0].src;
+    document.getElementById('speaker-name').innerHTML = elem.children[1].innerHTML;
+    document.getElementById('talk-name').innerHTML = attributes.name;
+    document.getElementById('talk-description').innerHTML = attributes.talkdesc;
+    if (attributes.talkvideo) {
+      document.getElementById('talk-video').href = attributes.talkvideo;
+    } else {
+      document.getElementById('talk-video').style.display = 'none';
+    }
+    document.getElementById('bio').innerHTML = attributes.bio;
+
+    document.getElementById('speakers').scrollIntoView({block: "start", behavior: "smooth"});
+  }
+}
+
